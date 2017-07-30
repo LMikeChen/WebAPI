f821f26876fc313d6ca95ec509aa4bcd43e3efc3
LMikeChen <mike.lchen@gmail.com>
2017-07-30 09:21:41 -0400

    first commit

---
+++ Take2/Take2/Controllers/FoodsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Take2.Models;
+
+namespace Take2.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        public object Get()
+        {
+            ReturnData data = new ReturnData() { ID = 1, Address = "123 1321", Name = "Mike" };
+            return data;
+        }
+    }
+}
---
+++ Take2/Take2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Take2.WebApiApplication" Language="C#" %>
